0f77c0a3dc357be35f11747f4bc69e174564df6c
LH-wang-code <2876254051@qq.com>
2025-04-12 21:58:10 +0800

    handle some questions on 2025.4.12

--- Sceniario2.cpp
+++ Sceniario2.cpp
@@ -224,75 +224,177 @@ void renderWall()
 	if (wallVAO == 0)
 	{
 
-		glm::vec3 pos1(-1.0, 1.0, 0.0);
-		glm::vec3 pos2(-1.0, -1.0, 0.0);
-		glm::vec3 pos3(1.0, -1.0, 0.0);
-		glm::vec3 pos4(1.0, 1.0, 0.0);
+		glm::vec3 position[6][4] = {
 
+			glm::vec3(-1.0f,1.0f,1.0f),glm::vec3(-1.0f,-1.0f,1.0f),glm::vec3(1.0f,-1.0f,1.0f),glm::vec3(1.0f,1.0f,1.0f),
+			glm::vec3(1.0f, 1.0f, -1.0f), glm::vec3(1.0f, -1.0f, -1.0f), glm::vec3(-1.0f, -1.0f, -1.0f), glm::vec3(-1.0f, 1.0f, -1.0f),
+			glm::vec3(-1.0f,1.0f,-1.0f),glm::vec3(-1.0f,-1.0f,-1.0f),glm::vec3(-1.0f,-1.0f,1.0f),glm::vec3(-1.0f,1.0f,1.0f),
+			glm::vec3(1.0f,1.0f,1.0f),glm::vec3(1.0f,-1.0f,1.0f),glm::vec3(1.0f,-1.0f,-1.0f),glm::vec3(1.0f,1.0f,-1.0f),
+			glm::vec3(-1.0f,1.0f,-1.0f),glm::vec3(-1.0f,1.0f,1.0f),glm::vec3(1.0f,1.0f,1.0f),glm::vec3(1.0f,1.0f,-1.0f),
+			glm::vec3(-1.0f,-1.0f,1.0f),glm::vec3(-1.0f,-1.0f,-1.0f),glm::vec3(1.0f,-1.0f,-1.0f),glm::vec3(1.0f,-1.0f,1.0f)
+
+		};
+		//TexCoords
 		glm::vec2 uv1(0.0, 1.0);
 		glm::vec2 uv2(0.0, 0.0);
 		glm::vec2 uv3(1.0, 0.0);
 		glm::vec2 uv4(1.0, 1.0);
-
-		glm::vec3 nm(0.0, 0.0, 1.0);
-
-
-		glm::vec3 tangent1, bitangent1;
-		glm::vec3 tangent2, bitangent2;
-
-		glm::vec3 edge1 = pos2 - pos1;
-		glm::vec3 edge2 = pos3 - pos1;
-		glm::vec2 deltaUV1 = uv2 - uv1;
-		glm::vec2 deltaUV2 = uv3 - uv1;
-
-		GLfloat f = 1.0f / (deltaUV1.x * deltaUV2.y - deltaUV2.x * deltaUV1.y);
-
-		tangent1.x = f * (deltaUV2.y * edge1.x - deltaUV1.y * edge2.x);
-		tangent1.y = f * (deltaUV2.y * edge1.y - deltaUV1.y * edge2.y);
-		tangent1.z = f * (deltaUV2.y * edge1.z - deltaUV1.y * edge2.z);
-		tangent1 = glm::normalize(tangent1);
-
-		bitangent1.x = f * (-deltaUV2.x * edge1.x + deltaUV1.x * edge2.x);
-		bitangent1.y = f * (-deltaUV2.x * edge1.y + deltaUV1.x * edge2.y);
-		bitangent1.z = f * (-deltaUV2.x * edge1.z + deltaUV1.x * edge2.z);
-		bitangent1 = glm::normalize(bitangent1);
-
-
-		edge1 = pos3 - pos1;
-		edge2 = pos4 - pos1;
-		deltaUV1 = uv3 - uv1;
-		deltaUV2 = uv4 - uv1;
-
-		f = 1.0f / (deltaUV1.x * deltaUV2.y - deltaUV2.x * deltaUV1.y);
-
-		tangent2.x = f * (deltaUV2.y * edge1.x - deltaUV1.y * edge2.x);
-		tangent2.y = f * (deltaUV2.y * edge1.y - deltaUV1.y * edge2.y);
-		tangent2.z = f * (deltaUV2.y * edge1.z - deltaUV1.y * edge2.z);
-		tangent2 = glm::normalize(tangent2);
-
-
-		bitangent2.x = f * (-deltaUV2.x * edge1.x + deltaUV1.x * edge2.x);
-		bitangent2.y = f * (-deltaUV2.x * edge1.y + deltaUV1.x * edge2.y);
-		bitangent2.z = f * (-deltaUV2.x * edge1.z + deltaUV1.x * edge2.z);
-		bitangent2 = glm::normalize(bitangent2);
-
-
-		GLfloat quadVertices[] = {
-
-			pos1.x, pos1.y, pos1.z, nm.x, nm.y, nm.z, uv1.x, uv1.y, tangent1.x, tangent1.y, tangent1.z, bitangent1.x, bitangent1.y, bitangent1.z,
-			pos2.x, pos2.y, pos2.z, nm.x, nm.y, nm.z, uv2.x, uv2.y, tangent1.x, tangent1.y, tangent1.z, bitangent1.x, bitangent1.y, bitangent1.z,
-			pos3.x, pos3.y, pos3.z, nm.x, nm.y, nm.z, uv3.x, uv3.y, tangent1.x, tangent1.y, tangent1.z, bitangent1.x, bitangent1.y, bitangent1.z,
-
-			pos1.x, pos1.y, pos1.z, nm.x, nm.y, nm.z, uv1.x, uv1.y, tangent2.x, tangent2.y, tangent2.z, bitangent2.x, bitangent2.y, bitangent2.z,
-			pos3.x, pos3.y, pos3.z, nm.x, nm.y, nm.z, uv3.x, uv3.y, tangent2.x, tangent2.y, tangent2.z, bitangent2.x, bitangent2.y, bitangent2.z,
-			pos4.x, pos4.y, pos4.z, nm.x, nm.y, nm.z, uv4.x, uv4.y, tangent2.x, tangent2.y, tangent2.z, bitangent2.x, bitangent2.y, bitangent2.z
+		//normal
+		glm::vec3 normals[6] = {
+			glm::vec3(0.0f,0.0f,1.0f),
+			glm::vec3(0.0f,0.0f,-1.0f),
+			glm::vec3(-1.0f,0.0f,0.0f),
+			glm::vec3(1.0f,0.0f,0.0f),
+			glm::vec3(0.0f,1.0f,0.0f),
+			glm::vec3(0.0f,-1.0f,0.0f)
 		};
+
+
+		const int facesRender = 5;
+		const int facesVertices = 6;
+		GLfloat Vertices[facesRender * facesVertices * 14];
+		int vertexIndex = 0;
+		for (int i = 0;i < facesRender;i++)
+		{
+			glm::vec3 tangent1, bitangent1;
+			glm::vec3 tangent2, bitangent2;
+
+			glm::vec3 edge1 = position[i][1] - position[i][0];
+			glm::vec3 edge2 = position[i][2] - position[i][0];
+			glm::vec2 deltaUV1 = uv2 - uv1;
+			glm::vec2 deltaUV2 = uv3 - uv1;
+			//tangent
+			GLfloat f = 1.0f / (deltaUV1.x * deltaUV2.y - deltaUV2.x * deltaUV1.y);
+			tangent1.x = f * (deltaUV2.y * edge1.x - deltaUV1.y * edge2.x);
+			tangent1.y = f * (deltaUV2.y * edge1.y - deltaUV1.y * edge2.y);
+			tangent1.z = f * (deltaUV2.y * edge1.z - deltaUV1.y * edge2.z);
+			tangent1 = glm::normalize(tangent1);
+			//bitangent
+			bitangent1.x = f * (-deltaUV2.x * edge1.x + deltaUV1.x * edge2.x);
+			bitangent1.y = f * (-deltaUV2.x * edge1.y + deltaUV1.x * edge2.y);
+			bitangent1.z = f * (-deltaUV2.x * edge1.z + deltaUV1.x * edge2.z);
+			bitangent1 = glm::normalize(bitangent1);
+
+			edge1 = position[i][2] - position[i][0];
+			edge2 = position[i][3] - position[i][0];
+			deltaUV1 = uv3 - uv1;
+			deltaUV2 = uv4 - uv1;
+			f = 1.0f / (deltaUV1.x * deltaUV2.y - deltaUV2.x * deltaUV1.y);
+
+			tangent2.x = f * (deltaUV2.y * edge1.x - deltaUV1.y * edge2.x);
+			tangent2.y = f * (deltaUV2.y * edge1.y - deltaUV1.y * edge2.y);
+			tangent2.z = f * (deltaUV2.y * edge1.z - deltaUV1.y * edge2.z);
+			tangent2 = glm::normalize(tangent2);
+
+			bitangent2.x = f * (-deltaUV2.x * edge1.x + deltaUV1.x * edge2.x);
+			bitangent2.y = f * (-deltaUV2.x * edge1.y + deltaUV1.x * edge2.y);
+			bitangent2.z = f * (-deltaUV2.x * edge1.z + deltaUV1.x * edge2.z);
+			bitangent2 = glm::normalize(bitangent2);
+			// 顶点1
+			Vertices[vertexIndex++] = position[i][0].x;
+			Vertices[vertexIndex++] = position[i][0].y;
+			Vertices[vertexIndex++] = position[i][0].z;
+			Vertices[vertexIndex++] = normals[i].x;
+			Vertices[vertexIndex++] = normals[i].y;
+			Vertices[vertexIndex++] = normals[i].z;
+			Vertices[vertexIndex++] = uv1.x;
+			Vertices[vertexIndex++] = uv1.y;
+			Vertices[vertexIndex++] = tangent1.x;
+			Vertices[vertexIndex++] = tangent1.y;
+			Vertices[vertexIndex++] = tangent1.z;
+			Vertices[vertexIndex++] = bitangent1.x;
+			Vertices[vertexIndex++] = bitangent1.y;
+			Vertices[vertexIndex++] = bitangent1.z;
+
+			// 顶点2
+			Vertices[vertexIndex++] = position[i][1].x;
+			Vertices[vertexIndex++] = position[i][1].y;
+			Vertices[vertexIndex++] = position[i][1].z;
+			Vertices[vertexIndex++] = normals[i].x;
+			Vertices[vertexIndex++] = normals[i].y;
+			Vertices[vertexIndex++] = normals[i].z;
+			Vertices[vertexIndex++] = uv2.x;
+			Vertices[vertexIndex++] = uv2.y;
+			Vertices[vertexIndex++] = tangent1.x;
+			Vertices[vertexIndex++] = tangent1.y;
+			Vertices[vertexIndex++] = tangent1.z;
+			Vertices[vertexIndex++] = bitangent1.x;
+			Vertices[vertexIndex++] = bitangent1.y;
+			Vertices[vertexIndex++] = bitangent1.z;
+
+			// 顶点3
+			Vertices[vertexIndex++] = position[i][2].x;
+			Vertices[vertexIndex++] = position[i][2].y;
+			Vertices[vertexIndex++] = position[i][2].z;
+			Vertices[vertexIndex++] = normals[i].x;
+			Vertices[vertexIndex++] = normals[i].y;
+			Vertices[vertexIndex++] = normals[i].z;
+			Vertices[vertexIndex++] = uv3.x;
+			Vertices[vertexIndex++] = uv3.y;
+			Vertices[vertexIndex++] = tangent1.x;
+			Vertices[vertexIndex++] = tangent1.y;
+			Vertices[vertexIndex++] = tangent1.z;
+			Vertices[vertexIndex++] = bitangent1.x;
+			Vertices[vertexIndex++] = bitangent1.y;
+			Vertices[vertexIndex++] = bitangent1.z;
+
+			// 第二个三角形
+			// 顶点1 (复用第一个顶点)
+			Vertices[vertexIndex++] = position[i][0].x;
+			Vertices[vertexIndex++] = position[i][0].y;
+			Vertices[vertexIndex++] = position[i][0].z;
+			Vertices[vertexIndex++] = normals[i].x;
+			Vertices[vertexIndex++] = normals[i].y;
+			Vertices[vertexIndex++] = normals[i].z;
+			Vertices[vertexIndex++] = uv1.x;
+			Vertices[vertexIndex++] = uv1.y;
+			Vertices[vertexIndex++] = tangent2.x;
+			Vertices[vertexIndex++] = tangent2.y;
+			Vertices[vertexIndex++] = tangent2.z;
+			Vertices[vertexIndex++] = bitangent2.x;
+			Vertices[vertexIndex++] = bitangent2.y;
+			Vertices[vertexIndex++] = bitangent2.z;
+
+			// 顶点3 (复用第一个三角形的第三个顶点)
+			Vertices[vertexIndex++] = position[i][2].x;
+			Vertices[vertexIndex++] = position[i][2].y;
+			Vertices[vertexIndex++] = position[i][2].z;
+			Vertices[vertexIndex++] = normals[i].x;
+			Vertices[vertexIndex++] = normals[i].y;
+			Vertices[vertexIndex++] = normals[i].z;
+			Vertices[vertexIndex++] = uv3.x;
+			Vertices[vertexIndex++] = uv3.y;
+			Vertices[vertexIndex++] = tangent2.x;
+			Vertices[vertexIndex++] = tangent2.y;
+			Vertices[vertexIndex++] = tangent2.z;
+			Vertices[vertexIndex++] = bitangent2.x;
+			Vertices[vertexIndex++] = bitangent2.y;
+			Vertices[vertexIndex++] = bitangent2.z;
+
+			// 顶点4
+			Vertices[vertexIndex++] = position[i][3].x;
+			Vertices[vertexIndex++] = position[i][3].y;
+			Vertices[vertexIndex++] = position[i][3].z;
+			Vertices[vertexIndex++] = normals[i].x;
+			Vertices[vertexIndex++] = normals[i].y;
+			Vertices[vertexIndex++] = normals[i].z;
+			Vertices[vertexIndex++] = uv4.x;
+			Vertices[vertexIndex++] = uv4.y;
+			Vertices[vertexIndex++] = tangent2.x;
+			Vertices[vertexIndex++] = tangent2.y;
+			Vertices[vertexIndex++] = tangent2.z;
+			Vertices[vertexIndex++] = bitangent2.x;
+			Vertices[vertexIndex++] = bitangent2.y;
+			Vertices[vertexIndex++] = bitangent2.z;
+		}
+
+
 
 		glGenVertexArrays(1, &wallVAO);
 		glGenBuffers(1, &wallVBO);
 		glBindVertexArray(wallVAO);
 		glBindBuffer(GL_ARRAY_BUFFER, wallVBO);
-		glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), &Vertices, GL_STATIC_DRAW);
 		glEnableVertexAttribArray(0);
 		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 14 * sizeof(GLfloat), (GLvoid*)0);
 		glEnableVertexAttribArray(1);
@@ -305,7 +407,7 @@ void renderWall()
 		glVertexAttribPointer(4, 3, GL_FLOAT, GL_FALSE, 14 * sizeof(GLfloat), (GLvoid*)(11 * sizeof(GLfloat)));
 	}
 	glBindVertexArray(wallVAO);
-	glDrawArrays(GL_TRIANGLES, 0, 6);
+	glDrawArrays(GL_TRIANGLES, 0, 30);
 	glBindVertexArray(0);
 }
 unsigned int loadCubemap(vector<std::string>faces)
@@ -604,7 +706,9 @@ int main()
 		//
 		//draw the wall
 		model = glm::mat4(1.0f);
-		model = glm::translate(model, glm::vec3(0.0f, 4.5f, 80.0f));
+		
+		model = glm::translate(model, glm::vec3(0.0f, 9.5f, 80.0f));
+		model = glm::scale(model, glm::vec3(10.0f));
 		simpleDepthShader.setMat4("model", model);
 		renderWall();
 
@@ -650,7 +754,11 @@ int main()
 
 
 		model = glm::mat4(1.0f);
-		model = glm::translate(model, glm::vec3(0.0f, 4.5f, 80.0f));
+		//model = glm::scale(model, glm::vec3(10.0f));
+		//model = glm::translate(model, glm::vec3(0.0f, 4.5f, 80.0f));
+		model = glm::translate(model, glm::vec3(0.0f, 9.5f, 80.0f));
+		model = glm::scale(model, glm::vec3(10.0f));
+		
 		wallShader.setMat4("model", model);
 		wallShader.setMat4("projection", projection);
 		wallShader.setMat4("view", view);
@@ -661,9 +769,9 @@ int main()
         glActiveTexture(GL_TEXTURE1);
         glBindTexture(GL_TEXTURE_2D, wallnormalMap);
         glActiveTexture(GL_TEXTURE2);
+
         glBindTexture(GL_TEXTURE_2D, walldispMap);
 		renderWall();
-
 		// 
 		//TreeShader.use();
 		//TreeShader.setMat4("projection", projection);
